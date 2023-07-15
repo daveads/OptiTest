46014fe2e7c7d1e5d06cefc00f5905cc86ebc8ee
daveads <the.daveads@gmail.com>
2023-07-15 22:34:25 +0100

    logger, error handling

--- cron_wrapper.py
+++ cron_wrapper.py
@@ -5,6 +5,10 @@ from datetime import datetime
 from dotenv import load_dotenv
 from utilities.current_time import get_current_date
 from utilities.email_sender import send_email
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)  # Set the log level to ERROR or the desired level
 
 async def main():
     load_dotenv(".env")
@@ -40,4 +44,10 @@ async def main():
     os.chmod(output_file, 0o644)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        log_file = "error.log"  # Specify the desired log file path
+        logging.basicConfig(filename=log_file, level=logging.ERROR)  # Configure logging to write ERROR level messages to the file
+
+        asyncio.run(main())
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
